829ff5d4fc9e43b0fb584d869eb80d325b7976dc
MagnoEfren <58579707+MagnoEfren@users.noreply.github.com>
2021-08-01 22:26:59 -0500

    Add files via upload

---
+++ Control de un Tanque/main.py
@@ -0,0 +1,79 @@
+#-*- coding: utf-8 -*-
+"""
+Created on Thu Sep 2020
+
+@author: Magno Efren
+https://www.youtube.com/c/MagnoEfren
+"""
+
+
+from tkinter import Tk, Canvas, Label, Button
+import time
+
+raiz = Tk()
+raiz.title("PROCESO LLENADO DE UN TANQUE DE 25000 L")
+
+raiz.resizable(0,0)
+raiz.geometry("500x300")
+raiz.config(bg="black")
+  
+tanque=Canvas(raiz,width=200,height=200,bg="black", highlightthickness=0)
+tanque.place(x=50,y=50)
+
+tanque.create_rectangle(140,20,10,180, fill="#E4E4E4")
+tanque.create_oval(10,170,140,190, fill= "#CAC6BE")
+tanque.create_rectangle(100,55,50,165, fill="white")
+tanque.create_rectangle(100,165,50,165, fill="blue")       
+tanque.create_oval(10,0,140,50, fill= "#CAC6BE")
+
+
+nombre1= Label(raiz, text="25000 L",bg="black",fg="white", font="Helvetica 12 bold" )    
+nombre1.pack()
+nombre1.place(x=190,y=65)    
+        
+nombre2= Label(raiz, text="0 L",bg="black",fg="white", font="Helvetica 12 bold" )    
+nombre2.pack()
+nombre2.place(x=190,y= 210)
+
+
+
+def llenado(): 
+    num=165     
+    for i in range(55,num):
+                   
+        tanque.create_rectangle(100,55,50,165, fill="white")
+        tanque.create_rectangle(100,num,50,165, fill="blue")       
+        tanque.update()
+
+        time.sleep(0.04) 
+        num = num - 1
+        N = int((i-64)*250)
+        
+        flecha= Label(raiz, text="←",bg="black",fg="white", font="Helvetica 18 bold" )    
+        flecha.pack()
+        flecha.place(x=190, y= num + 30)   
+       
+        indicadorN= Label(raiz, text= (f"Nivel actual = {N}"),bg="black",fg="white", font="Helvetica 10 bold" )   
+        indicadorN.pack()
+        indicadorN.place(x=300, y= 70)
+
+
+
+
+def salir():
+    raiz.destroy()
+    raiz.quit()
+
+
+boton = Button(raiz, bg ="magenta", text ="LLENAR ",font="Helvetica 10 bold", command =llenado, width = 15)
+boton.pack()
+boton.place(x=300,y=120)  
+
+
+
+boton3 = Button(raiz, bg ="red", text ="  SALIR ",font="Helvetica 10 bold", command =salir,width =15)
+boton3.pack()
+boton3.place(x=300,y=220)    
+
+
+raiz.mainloop()
